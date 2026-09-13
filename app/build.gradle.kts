ae23143ce9ecebeb7644893ce9169821d9763efb
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2026-09-13 19:52:57 +0530

    Bumped to 3v

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.f2l.downloader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "2.5"
+        versionCode = 4
+        versionName = "3.0"
     }
 
     buildFeatures {
