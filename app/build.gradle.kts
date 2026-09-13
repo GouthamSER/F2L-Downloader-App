0df1a318a109f1a55be57d34b895e76dadb11a02
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2026-09-13 10:02:46 +0530

    Bumb to version 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.f2l.downloader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     buildFeatures {
